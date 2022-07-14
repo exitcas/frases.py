93acf1a1b94b61cc6ed9cc5e4815c7647c6ca315
Luqaska <lucas@atico.ga>
2022-07-14 19:33:22 -0300

    0.0.1 => 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 setuptools.setup(
   name="frases",
   packages=["frases"],
-  version="0.0.1",
+  version="0.1.0",
   author="Luqaska",
   description="API de frases",
   url="https://github.com/Luqaska/frases.py",
