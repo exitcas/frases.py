d4c022b9b3a90d0088004e8f8a12b6f87654e34e
Luqaska <lucas@atico.ga>
2022-07-14 19:44:43 -0300

    v0.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
+
+f = open("README.md", "r")
+long_description = f.read()
+f.close()
+
 setuptools.setup(
   name="frases",
   packages=["frases"],
   version="0.1.0",
   author="Luqaska",
   description="API de frases",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   url="https://github.com/Luqaska/frases.py",
+  project_urls={
+    "Bug Tracker": "https://github.com/Luqaska/frases.py/issues"
+  }
   requires=["requests"],
   keywords=["API", "frases"]
 )
