a47d94bca5400f385b4c91d69523b6ed689d0624
Luqaska <lucas@atico.ga>
2022-07-14 19:23:10 -0300

    Actulizar "setup.py"

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import setuptools
 setuptools.setup(
   name="frases",
+  packages=["frases"],
   version="0.0.1",
   author="Luqaska",
   description="API de frases",
-  packages=["frases"]
+  url="https://github.com/Luqaska/frases.py",
+  requires=["requests"],
+  keywords=["API", "frases"]
 )
